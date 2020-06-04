6181aba5dfaad6a846b44d2bdcb193360785c809
nt <nt@hbt.de>
2020-06-04 09:44:23 +0200

    Added IDENTITY to ReportData

--- src/main/java/de/hbt/model/ReportData.java
+++ src/main/java/de/hbt/model/ReportData.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 public class ReportData {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
     @Column(name = "initials")
